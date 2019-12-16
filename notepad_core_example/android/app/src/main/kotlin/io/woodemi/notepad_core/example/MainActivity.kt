b5c01c86ce598d1a5e5ed9acc6f4c258f5412fb0
wangkun <wangkun@woodemi.com>
2019-12-16 21:06:33 +0800

    Config Android project
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- notepad_core_example/android/app/src/main/kotlin/io/woodemi/notepad_core/example/MainActivity.kt
+++ notepad_core_example/android/app/src/main/kotlin/io/woodemi/notepad_core/example/MainActivity.kt
@@ -1,12 +1,12 @@
-package com.example.notepad_core_example
+package io.woodemi.notepad_core.example
 
-import androidx.annotation.NonNull;
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
 }
