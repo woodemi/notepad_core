da1cbc71b3ef7793365fd9be58df38e316d86feb
wangkun <wangkun@woodemi.com>
2019-12-16 19:39:33 +0800

    Config Web project
    Signed-off-by: wangkun <wangkun@woodemi.com> (+1 squashed commit)
    Squashed commits:
    [b882dea] WIP: Config Web project

---
+++ notepad_core_web/lib/notepad_core_web.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+class NotepadCorePlugin {
+  static void registerWith(Registrar registrar) {
+    // TODO
+  }
+}
