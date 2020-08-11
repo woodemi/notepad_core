3edad4072379a6d4910a1de0fe76611e720a9460
lxd-Super-main <lxd_super_main@126.com>
2020-08-11 15:16:22 +0800

    Update notepad_core_example

--- notepad_core_example/android/app/src/main/kotlin/io/woodemi/notepad_core/example/MainActivity.kt
+++ notepad_core_example/android/app/src/main/kotlin/io/woodemi/notepad_core/example/MainActivity.kt
@@ -1,12 +1,12 @@
 package io.woodemi.notepad_core.example
 
+import android.os.Bundle
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        super.configureFlutterEngine(flutterEngine);
     }
 }
